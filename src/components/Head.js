53bd01b27e2f74ef03ac15d6a79323345d201223
stormcloud266 <allprt89@gmail.com>
2018-09-08 16:53:10 -0600

    adding json ld

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 
-const Head = ({data}) => {
+const Head = ({ data }) => {
   return (
     <Helmet
       title={data.site.siteMetadata.title}
@@ -17,7 +17,26 @@ const Head = ({data}) => {
     >
       <script type="application/ld+json">{`
         {
-            "@context": "http://schema.org"
+          "@context": "http://schema.org",
+          "@type": "LocalBusiness",
+          "address": {
+          "@type": "PostalAddress",
+          "addressLocality": "Cody",
+          "addressRegion": "WY",
+          "postalCode":"82414"
+          },
+          "description": "Get found online with a fast, responsive website built from the ground up. Tawnee Allport is a freelance web developer and designer based out of Wyoming.",
+          "name": "Modern Web Developer | Tawnee Allport",
+          "telephone": "857-209-4930",
+          "image":"https://www.tawneeallport.com/pfp.png",
+          "geo": {
+          "@type": "GeoCoordinates",
+          "latitude": "40.74",
+          "longitude": "-73.98"
+          },
+          "sameAs" : [ "https://twitter.com/TA_Coding",
+          "https://github.com/stormcloud266",
+          "https://codepen.io/stormcloud266"]
         }
     `}</script>
 
